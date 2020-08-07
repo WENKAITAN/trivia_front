ded0f3f9fd6b9616b6af15afc15302e075f084ac
WENKAI TAN <52226880+WenkaiTan@users.noreply.github.com>
2020-08-07 15:48:01 -0400

    Start with create-react-app

---
+++ build/precache-manifest.b0c194ea477819f6f585fa884bf52588.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae04cbd2eb5c8e4a8bd4bdbbb3aa670c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f51f46cf040183fe38a4",
+    "url": "/static/css/main.ee25cf69.chunk.css"
+  },
+  {
+    "revision": "072f3e95e3a0453af94c",
+    "url": "/static/js/2.6d5d0db4.chunk.js"
+  },
+  {
+    "revision": "f51f46cf040183fe38a4",
+    "url": "/static/js/main.3799f62a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
