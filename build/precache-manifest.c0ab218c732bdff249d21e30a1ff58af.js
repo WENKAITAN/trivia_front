88778b0cc57d130a33704515c19998a466537ec4
WENKAI TAN <52226880+WenkaiTan@users.noreply.github.com>
2020-08-17 14:46:08 -0400

    update

---
+++ build/precache-manifest.c0ab218c732bdff249d21e30a1ff58af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4913218fac6fa2802bc7b9176bc8c5df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9044965dea6344bd1ef1",
+    "url": "/static/css/main.ee25cf69.chunk.css"
+  },
+  {
+    "revision": "072f3e95e3a0453af94c",
+    "url": "/static/js/2.6d5d0db4.chunk.js"
+  },
+  {
+    "revision": "9044965dea6344bd1ef1",
+    "url": "/static/js/main.5383034f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
